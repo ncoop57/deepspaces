22c7899ca2aa1c1531f1dbde9ca51a3db23e21e7
BhavyanshM <bhavyanshmishra@gmail.com>
2018-03-03 04:02:52 -0600

    Add the save mongodb schema database function ...

--- webui-ms/routes/index.js
+++ webui-ms/routes/index.js
@@ -10,12 +10,12 @@ router.get("/dashboard", function(req, res) {
 router.post("/add_space", function(req, res) {
     console.log("adding space", req.body);
     res.send({'SUCCESS': true});
-    for(var space in req.body){
-        const new_space = new Space({space_id: space.space_id,
+    for(var i = 0; i < req.body.length; i++){
+        const new_space = new Space({space_id: req.body[i].space_id,
                                     is_available: false,
-                                    type: space.type,
-                                    x_coord: space.x_coord,
-                                    y_coord: space.y_coord,
+                                    type: req.body[i].type,
+                                    x_coord: req.body[i].x_coord,
+                                    y_coord: req.body[i].y_coord,
                                     percentage: 0});
         new_space.save(function(err, new_space) {
             if (err) return console.error(err);
