77391a9409eb6f44744e3e39d14e6c17f2419ea4
BhavyanshM <bhavyanshmishra@gmail.com>
2018-03-03 03:36:01 -0600

    Add the javascript functions and Ajax POST request to dashboard UI

--- webui-ms/routes/index.js
+++ webui-ms/routes/index.js
@@ -8,17 +8,18 @@ router.get("/dashboard", function(req, res) {
 });
 
 router.post("/add_space", function(req, res) {
-    console.log("adding space", req.body.space_id);
-    const new_space = new Space({space_id: req.body.space_id,
-                                is_available: false,
-                                type: req.body.type,
-                                x_coord: req.body.x_coord,
-                                y_coord: req.body.y_coord,
-                                percentage: 0});
+    console.log("adding space", req.body);
+    res.send('SUCCESS');
+    // const new_space = new Space({space_id: req.body.space_id,
+    //                             is_available: false,
+    //                             type: req.body.type,
+    //                             x_coord: req.body.x_coord,
+    //                             y_coord: req.body.y_coord,
+    //                             percentage: 0});
 
-    new_space.save(function(err, new_space) {
-        if (err) return console.error(err);
-    });
+    // new_space.save(function(err, new_space) {
+    //     if (err) return console.error(err);
+    // });
 });
 
 module.exports = router;
